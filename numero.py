2360b728b568b22654063d5b099cc289042c85eb
arq62 <62590607+arq62@users.noreply.github.com>
2020-06-17 17:21:59 +0530

    Update numero.py

--- numero.py
+++ numero.py
@@ -71,13 +71,11 @@ def namelength(s):
 import matplotlib.pyplot as plt
 import numpy as np
 
-# 100 linearly spaced numbers
+
 x = np.linspace(0,10**3,10**3-1)
 
-# the function, which is y = x^2 here
 y = namelength(x)
 
-# setting the axes at the centre
 fig = plt.figure()
 ax = fig.add_subplot(1, 1, 1)
 ax.spines['left'].set_position('center')
@@ -87,8 +85,7 @@ ax.spines['top'].set_color('none')
 ax.xaxis.set_ticks_position('bottom')
 ax.yaxis.set_ticks_position('left')
 
-# plot the function
+
 plt.plot(x,y, 'r')
 
-# show the plot
 plt.show()
