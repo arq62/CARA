158ac4f8c89c8af341525417699e58f5f0ebfc0e
arq62 <62590607+arq62@users.noreply.github.com>
2020-06-17 17:16:37 +0530

    Add files via upload

--- numm.py
+++ numm.py
@@ -1,7 +1,5 @@
 def namer(inp):
-	if inp == 0:
-		return 'zero'
-	else:
+	
 		numstr = str(inp)
 		numstr.lstrip('0')
 
@@ -69,16 +67,28 @@ def namelength(s):
 	nomlen = len(k) - k.count(' ')
 	return nomlen
 
-def numloop(n):
-	while namelength(n) != n:
-		n = namelength(n)
-	return n
-			
 
-trange = input('Range? ')
-j = 0
-for i in range(int(trange)):
-	if numloop(i) == 4:
-		j +=1
+import matplotlib.pyplot as plt
+import numpy as np
 
-print('CARA valid for {} numbers in given range.'.format(j))
+# 100 linearly spaced numbers
+x = np.linspace(0,10**3,10**3-1)
+
+# the function, which is y = x^2 here
+y = namelength(x)
+
+# setting the axes at the centre
+fig = plt.figure()
+ax = fig.add_subplot(1, 1, 1)
+ax.spines['left'].set_position('center')
+ax.spines['bottom'].set_position('zero')
+ax.spines['right'].set_color('none')
+ax.spines['top'].set_color('none')
+ax.xaxis.set_ticks_position('bottom')
+ax.yaxis.set_ticks_position('left')
+
+# plot the function
+plt.plot(x,y, 'r')
+
+# show the plot
+plt.show()
